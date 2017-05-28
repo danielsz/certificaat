8ecbbccef5f7a952136a93e93bd6259018bf33ff
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2017-05-29 01:57:05 +0300

    All challenges
    - Explain
    - Show instructions

--- src/certificaat/acme4j/challenge.clj
+++ src/certificaat/acme4j/challenge.clj
@@ -8,23 +8,53 @@
            [org.shredzone.acme4j Status]
            [org.shredzone.acme4j.exception AcmeRetryAfterException AcmeServerException]))
 
-(defn http [challenge domain]
-  (->> ["Please create a file in your web server's base directory."
-        (str  "It must be reachable at:" (str "http://" domain  "/.well-known/acme-challenge/" (.getToken challenge)))
-        (str "File name:" (.getToken challenge))
-        (str "Content:" (.getAuthorization challenge))
-        "The file must not contain any leading or trailing whitespaces or line breaks!"]
+(defn tls-sni-01 [challenge domain]
+  (->> ["With the tls-sni-01 challenge, you prove to the CA that you are able to control the web server of the domain to be authorized, by letting it respond to a SNI request with a specific self-signed cert."
+        (str "You need to create a self-signed certificate with " (.getSubject challenge) " set as Subject Alternative Name.")
+        "After that, configure your web server so it will use this certificate on a SNI request to the subject."
+        "The challenge is completed when the CA was able to send the SNI request and get the correct certificate in return."
+        "More information at https://github.com/shred/acme4j/blob/608dbb6fb97d248ae20733289bfc8773e1f83ea4/src/site/markdown/challenge/tls-sni-01.md"]
        (str/join \newline)))
 
-(defn dns [challenge domain]
+(defn tls-sni-02 [challenge domain]
+  (->> ["With the tls-sni-02 challenge, you prove to the CA that you are able to control the web server of the domain to be authorized, by letting it respond to a SNI request with a specific self-signed cert." 
+        (str "You need to create a self-signed certificate with both " (.getSubject challenge) " and " (.getSanB challenge) " set as Subject Alternative Name.")
+        "After that, configure your web server so it will use this certificate on a SNI request to the subject."
+        "The challenge is completed when the CA was able to send the SNI request and get the correct certificate in return."
+        "More information at https://github.com/shred/acme4j/blob/608dbb6fb97d248ae20733289bfc8773e1f83ea4/src/site/markdown/challenge/tls-sni-02.md"]
+       (str/join \newline)))
+
+(defn oob-01 [challenge domain]
+  (->> ["The oob-01 challenge is an out of band challenge that is used when there is no automatic way of validating ownership of a domain. The client is instead required to perform actions outside of the ACME protocol." 
+        (str "You need to go to  " (.getValidationUrl challenge) " to receive further instructions about the actions to be taken")
+        "The challenge must be triggered before the URL is opened in a browser."
+        "Due to the nature of this challenge, it may take a considerable amount of time until its state changes to VALID."]
+       (str/join \newline)))
+
+(defn http-01 [challenge domain]
+  (->> ["Please create a file in your web server's base directory."
+        (str  "It must be reachable at:" (str "http://" domain  "/.well-known/acme-challenge/" (.getToken challenge)))
+        (str "File name: " (.getToken challenge))
+        (str "Content: " (.getAuthorization challenge))
+        "The file must not contain any leading or trailing whitespaces or line breaks!"
+        "The Content-Type header must be either text/plain or absent"
+        "The request is sent to port 80 only. There is no way to choose a different port, for security reasons."
+        "The challenge is completed when the CA was able to download that file and found content in it."]
+       (str/join \newline)))
+
+(defn dns-01 [challenge domain]
   (->> ["Please create a TXT record in your DNS settings:"
-       (str "_acme-challenge." domain " IN TXT " (.getDigest challenge))]
+        (str "_acme-challenge." domain " IN TXT " (.getDigest challenge))
+        "The challenge is completed when the CA was able to fetch the TXT record and got the correct digest returned."]
       (str/join \newline)))
 
 (defn explain [challenge domain]
   (case (.getType challenge)
-    "dns-01" (dns challenge domain)
-    "http-01" (http challenge domain)))
+    "dns-01" (dns-01 challenge domain)
+    "http-01" (http-01 challenge domain)
+    "tls-sni-01" (tls-sni-01 challenge domain)
+    "tls-sni-02" (tls-sni-02 challenge domain)
+    "oob-01" (oob-01 challenge domain)))
 
 (defn find [auth challenges]
   (.findCombination auth (into-array String challenges)))
